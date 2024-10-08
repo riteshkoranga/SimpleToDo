06e66fcf11df15f7e88ffba810f55987a437ab5b
riteshkoranga <riteshkoranga5@gmail.com>
2024-10-08 19:18:44 +0530

    backend with static build files from frontend,ready to be deployed

---
+++ model/Task.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const taskschema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = mongoose.model("Task", taskschema);
